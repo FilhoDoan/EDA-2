3a734444fb4a679c16c43cbe52bfd77ec6ec5521
FilhoDoan <doangalvaofilho@gmail.com>
2024-04-05 15:16:32 -0300

    adicionando resolução completa cinema

--- Listas/Lista_01/Lugares_cinema.c
+++ Listas/Lista_01/Lugares_cinema.c
@@ -1,35 +1,47 @@
 #include <stdio.h>
-#include <string.h>
 
 int main(){
-    int f, l,numero;    
-    char letra;
-    scanf("%d %d", &f, &l);
-    int dimensao = f*l;
-    
-    char matriz[f][l];
-    
-    for(int i = 0; i < f ; i ++){
-        for(int j = 0 ; j < l; j++){
-            matriz[i][j] = '-'; 
-        }
-    }    
-    
-    while(scanf(" %c%d", &letra, &numero) != EOF ){
-        matriz[letra - 'A'][numero - 1 ] = 'X'; 
+    int fila, lugar, numLugar; 
+    char letra ; 
+
+    scanf("%d %d", &fila, &lugar);
+
+    char salaCinema[fila][lugar]; 
+
+    for(int i = 0; i < fila; i ++){
+        for(int j = 0; j < lugar ; j++){
+            salaCinema[i][j] = '-';  
+        }   
     }
-    printf("  ");
-    for(int i = 1 ; i <= f ; i++){
-        printf("%02d ", i);
+
+    while(scanf(" %c%d", &letra, &numLugar) != EOF){
+        int f = letra - 'A';
+        int l = numLugar - 1;
+        salaCinema[f][l] = 'X';
+    }
+
+
+    printf("   "); 
+    for(int i = 0; i < lugar ; i++){
+        printf("%02d ", i+1);
     }
     printf("\n");
-    
-    for(int i = f-1; i >=0 ; i--){
+    for(int i = fila -1; i >= 0; i--){
         printf("%c ", 'A' + i);
-        for(int j = 0 ; j < l ; j++){
-            printf("%c%c ", matriz[i][j], matriz[i][j]);
+        for(int j = 0; j < lugar ; j++){
+            printf(" %c%c",salaCinema[i][j],salaCinema[i][j]);
         }
         printf("\n");
+
     }
+
+    return 0 ; 
+
+
 }
-     
+
+
+
+
+
+
